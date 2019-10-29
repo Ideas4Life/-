ad40aec803258537807ee209c1485bd5dad2e5e3
Роман <kraw4enko.tat@yandex.ru>
2019-10-29 20:25:49 +0300

    Демо версия интерфейса окна авторизации

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace BRS_Hostel
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new loginForm());
         }
     }
 }
---
+++ loginForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BRS_Hostel
+{
+    public partial class loginForm : Form
+    {
+        public loginForm()
+        {
+            InitializeComponent();
+            this.passField.AutoSize = false;
+            this.passField.Size = new Size(this.passField.Size.Width,41);
+            this.loginField.Size = new Size(this.loginField.Size.Width, 41);
+        }
+
+        private void сloseButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void сloseButton_MouseEnter(object sender, EventArgs e)
+        {
+            сloseButton.ForeColor = Color.Black;
+        }
+
+        private void сloseButtom_MouseLeave(object sender, EventArgs e)
+        {
+            сloseButton.ForeColor = Color.White;
+        }
+        Point lastPoint;
+        private void upPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void upPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+    }
+}
